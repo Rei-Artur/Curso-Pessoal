609a52c860f7b4f434ff45c55f3bb0ca9955009a
Rei-Artur <72677873+Rei-Artur@users.noreply.github.com>
2025-12-08 19:33:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function verificarSenha() {
+    const senha = document.getElementById('senha').value;
+    if(senha === '1234') {
+        document.getElementById('login-screen').classList.add('hidden');
+        document.getElementById('main-content').classList.remove('hidden');
+    } else {
+        document.getElementById('erro-senha').innerText = 'Senha incorreta!';
+    }
+}
+
+function mostrarModulo(n) {
+    const modulos = document.querySelectorAll('.modulo');
+    modulos.forEach(m => m.classList.add('hidden'));
+    document.getElementById(`modulo${n}`).classList.remove('hidden');
+}
 
